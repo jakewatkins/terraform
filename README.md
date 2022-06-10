b71fa205298ee68a90cf34095a752f0159636fde
jakewatkins <jake.watkins@gmail.com>
2022-06-10 06:29:02 -0500

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # terraform
 repo for terraform stuff I'm learning
+
+## vm-win-pro
+Sets up a vm running Windows 11 Pro w/ a 1TB C drive.
+-Enables PS Remote
+-Enable automatic updates
+-Give it a public IP address
+
---
+++ vm-win-pro/terraform/Variables.tf
@@ -0,0 +1,30 @@
+
+variable "azRegion" {
+    default = "centralus"
+    description = "location where the resource is hosted"
+}
+
+variable "rgName" {
+    default = "Sprinter"
+    description = "name of the resource group"
+}
+
+variable "AvailabilitySetName" {
+    default = "ASSprinter"
+    description = "name of availability set"
+}
+
+variable "machineName" {
+  default = "vmjkwWS20220609"
+  description = "name of the vm"
+}
+
+variable "username" {
+  default = "sysAdmin"
+  description = "admin user name"
+}
+
+variable "password" {
+    default = "P@ssw0rd1234!"
+    description = "admin password - should change"  
+}
